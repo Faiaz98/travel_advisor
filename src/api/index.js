66640cde4dae9ad03920d6cfb141832e82703e3d
Md. Iram Faiaz Khan <iram.faiaz99@gmail.com>
2022-02-25 04:37:32 +0600

    init

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
 
 
 
-export const getPlacesData = async(sw, ne) => {
+export const getPlacesData = async(type, sw, ne) => {
     try {
-        const { data: { data } } = await axios.get(URL, {
+        const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
 
             params: {
                 bl_latitude: sw.lat,
@@ -17,10 +16,25 @@ export const getPlacesData = async(sw, ne) => {
             },
             headers: {
                 'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-                'x-rapidapi-key': 'f77bbcb256msh202b0f03f2b7c6dp1d5051jsnb51574230526'
+                'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
             }
         });
 
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getWeatherData = async(lat, lng) => {
+    try {
+        const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+            params: { lon: lng, lat: lat, },
+            headers: {
+                'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+                'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+            }
+        });
         return data;
     } catch (error) {
         console.log(error)
